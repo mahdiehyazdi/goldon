06c79373e0f50c27247e8b9e1ff8cf5787d83545
mahdieh yazdi <mahdiehyazdi801@gmail.com>
2022-08-17 13:44:07 +0430

    connect db to client app page

--- client/src/Components/Pages/App/App.js
+++ client/src/Components/Pages/App/App.js
@@ -4,12 +4,48 @@ import "./App.responsive.css";
 import "./App.css";
 import Header from "../../UI/molecules/Header/Header";
 import Footer from "../../UI/molecules/Footer/Footer";
+import axios from "../../../utils/axios";
 
 // import SVG
 import { ReactComponent as ImageApp } from "./SVG/imageApp.svg";
 import { ReactComponent as Filter } from "./SVG/filter.svg";
+import { ReactComponent as Menu } from "./SVG/menu.svg";
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selfWorthGet: {},
+        };
+    }
+    async componentDidMount() {
+        try {
+            let { data } = await axios.get("goldon/selfworth/getSelfKnowledge", {
+                // withCredentials: true,
+            });
+            let { session, courseTeacher, fileType, courseTime, courseTopics, icon, link, courseRecommended } = data.SelfKnowledge;
+            this.setState(
+                {
+                    selfWorthGet: {
+                        session,
+                        courseTeacher,
+                        fileType,
+                        courseTime,
+                        courseTopics,
+                        icon,
+                        link,
+                        courseRecommended,
+                    },
+                },
+                () => console.log(this.state.selfWorthGet)
+            );
+        } catch (error) {
+            console.log(error);
+        }
+        if (this.state.selfWorthGet.fileType === 1) {
+            this.state.selfWorthGet.fileType = "صوتی";
+        }
+    }
     render() {
         return (
             <Grid container>
@@ -40,23 +76,65 @@ export default class App extends Component {
                     <p>مشاهده همه</p>
                 </Grid>
                 <Grid xs={12} item className="App__item">
-                    <Grid xs={5.5} item className="App__item__box" id="courseRecommended">
-                        <a href="goldon/#/app/assessment">
-                            <span className="App__item__box__title">جذابـیـت درونـی</span>
-                        </a>
+                    <Grid xs={5} item className="App__item__box" id={this.state.selfWorthGet.courseRecommended === true ? "courseRecommended" : ""}>
+                        <Grid item xs={12} className="App__item__box__menu">
+                            <Menu fill={this.state.selfWorthGet.courseRecommended === true ? "white" : "#5B6762"} />
+                        </Grid>
+                        <Grid item xs={12} className="App__item__box__header">
+                            <Grid item xs={8} className="">
+                                <a href="goldon/#/app/assessment">
+                                    <span className="App__item__box__title">{this.state.selfWorthGet.session ? this.state.selfWorthGet.session.courseName : "عنوان تعریف نشده است"}</span>
+                                    <br />
+                                    <span className="App__item__box__description">{this.state.selfWorthGet.session ? this.state.selfWorthGet.session.courseDescription : "عنوان تعریف نشده است"}</span>
+                                </a>
+                            </Grid>
+                            <Grid item xs={3} className="">
+                                <span className="App__item__box__title">{this.state.selfWorthGet.icon ? this.state.selfWorthGet.icon : "آیکون موجود نیست"}</span>
+                            </Grid>
+                        </Grid>
+
+                        <Grid item xs={12} className="App__item__box__info">
+                            <Grid item xs={8} className="">
+                                <span className="">تعداد جلسات : {this.state.selfWorthGet.session ? this.state.selfWorthGet.session.sessionCount : "عنوان تعریف نشده است"}</span>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <span>نوع فایل {this.state.selfWorthGet.fileType ? this.state.selfWorthGet.fileType : "تعریف نشده است"}</span>
+                            </Grid>
+                        </Grid>
+
+                        <Grid item xs={12} className="App__item__box__time">
+                            <span className="">زمان برنامه : {this.state.selfWorthGet.courseTime ? this.state.selfWorthGet.courseTime.hour + "ساعت و " + this.state.selfWorthGet.courseTime.minutes + "دقیقه" : "عنوان تعریف نشده است"}</span>
+                        </Grid>
+                        <Grid item xs={12} className="App__item__box__link">
+                            <a>ورود به سایت {this.state.selfWorthGet.link.webSite.name ? this.state.selfWorthGet.link.webSite.name : null}</a>
+                        </Grid>
                     </Grid>
-                    <Grid xs={5.5} item className="App__item__box">
-                        <span className="App__item__box__title">جذابـیـت درونـی</span>
+                    <Grid xs={5} item className="App__item__box" id={this.state.selfWorthGet.courseRecommended === true ? "courseRecommended" : ""}>
+                        <Grid item xs={12} className="App__item__box__menu">
+                            <Menu fill={this.state.selfWorthGet.courseRecommended === true ? "white" : "#5B6762"} />
+                        </Grid>
+                        <Grid item xs={12} className="App__item__box__header">
+                            <Grid item xs={8} className="">
+                                <a href="goldon/#/app/assessment">
+                                    <span className="App__item__box__title">{this.state.selfWorthGet.session ? this.state.selfWorthGet.session.courseName : "عنوان تعریف نشده است"}</span>
+                                    <br />
+                                    <span className="App__item__box__description">{this.state.selfWorthGet.session ? this.state.selfWorthGet.session.courseDescription : "عنوان تعریف نشده است"}</span>
+                                </a>
+                            </Grid>
+                            <Grid item xs={3} className="">
+                                <span className="App__item__box__title">{this.state.selfWorthGet.icon ? this.state.selfWorthGet.icon : "آیکون موجود نیست"}</span>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
-                <Grid xs={12} item className="App__item">
+                {/* <Grid xs={12} item className="App__item">
                     <Grid xs={5.5} item className="App__item__box">
                         <span className="App__item__box__title">جذابـیـت درونـی</span>
                     </Grid>
                     <Grid xs={5.5} item className="App__item__box">
                         <span className="App__item__box__title">جذابـیـت درونـی</span>
                     </Grid>
-                </Grid>
+                </Grid> */}
             </Grid>
         );
     }
--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,5 +1,4 @@
-import axios from 'axios'
-
+import axios from "axios";
 export default axios.create({
     baseURL:'http://localhost:3001/',
     headers:{'X-Requeste-from':'Mahdieh'},
